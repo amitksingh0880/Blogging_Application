3822dfa549057172bcbf39049b2ab1878e81ae7e
amitksingh0880 <amitksingh.1204@gmail.com>
2024-08-23 22:43:52 +0530

    basic Auth: form based to javascript alert- change

--- src/main/java/com/spring/blogging/config/SecurityConfig.java
+++ src/main/java/com/spring/blogging/config/SecurityConfig.java
@@ -27,9 +27,9 @@ public class SecurityConfig {
         return http.build();
     }
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
 
 }
